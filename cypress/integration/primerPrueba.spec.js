4c144e7374521f36d810d8a1682558cf6e9e659a
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2022-02-16 22:13:31 -0600

    primer prueba y estructura que se pueden usar las pruebas

---
+++ cypress/integration/primerPrueba.spec.js
@@ -0,0 +1,38 @@
+describe('Primer Suite de Pruebas', () => {
+
+    describe('Primer Suite de Pruebas', () => {
+        it('primer prueba', () => {
+            cy.visit('https://platzi.com');
+        });
+        it('segunda prueba', () => {
+            cy.visit('https://platzi.com');
+        });
+        it('tercera prueba', () => {
+            cy.visit('https://platzi.com');
+        });
+
+    });
+    it('primer prueba', () => {
+        cy.visit('https://platzi.com');
+    });
+    it('segunda prueba', () => {
+        cy.visit('https://platzi.com');
+    });
+    it('tercera prueba', () => {
+        cy.visit('https://platzi.com');
+    });
+
+});
+
+describe('Segunda Suite de Pruebas', () => {
+    it('primer prueba', () => {
+        cy.visit('https://platzi.com');
+    });
+    it('segunda prueba', () => {
+        cy.visit('https://platzi.com');
+    });
+    it('tercera prueba', () => {
+        cy.visit('https://platzi.com');
+    });
+
+});
