acfa64a81de7751c0ccf0d223b5f6bed9dd2c06c
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2022-02-17 22:19:09 -0600

    hooks

--- cypress/integration/aserciones.spec.js
+++ cypress/integration/aserciones.spec.js
@@ -1,6 +1,24 @@
+// before(() => {
+// 	//solo si se quiere correr antes de los dos bloques
+// 	cy.visit('/automation-practice-form')
+// })
+
+
 describe('Aserciones', () => {
-	it('Navega a sitio', () => {
+	// beforeEach(() => {
+	// 	cy.visit('/automation-practice-form')
+	// })
+	before(() => {
 		cy.visit('/automation-practice-form')
+	})
+
+	afterEach(() => {
+		cy.reload()
+	})
+
+	after(() => {
+		// si quiero continuar con otro bloque en otra url
+		cy.visit('/')
 	})
 	it('Aserciones', () => {
 		cy.url().should('include', 'demoqa.com')
@@ -15,16 +33,32 @@ describe('Aserciones', () => {
 	})
 
 	it('Aserciones 2', () => {
-		cy.visit('/automation-practice-form')
 		cy.get('#firstName').then((element) => {
 			expect(element).to.be.visible
 			expect(element).to.have.attr('placeholder', 'First Name')
 		})
 	})
 	it('Aserciones 3', () => {
-		cy.visit('/automation-practice-form')
 		cy.get('#firstName').then((element) => {
 			assert.equal(element.attr('placeholder'), 'First Name')
 		})
+	})
+})
+
+describe('Segundo Bloque', () => {
+	beforeEach(() => {
+		cy.visit('/automation-practice-form')
+	})
+
+	it('Aserciones', () => {
+		cy.url().should('include', 'demoqa.com')
+
+		cy.get('#firstName')
+			.should('be.visible')
+			.should('have.attr', 'placeholder', 'First Name')
+
+		cy.get('#firstName')
+			.should('be.visible')
+			.and('have.attr', 'placeholder', 'First Name')
 	})
 })
