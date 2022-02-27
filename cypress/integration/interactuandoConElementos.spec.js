15a8321133ef4fa038923c55af07783d3c7960c9
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2022-02-26 20:10:47 -0600

    interactuando con inputs

--- cypress/integration/interactuandoConElementos.spec.js
+++ cypress/integration/interactuandoConElementos.spec.js
@@ -41,7 +41,7 @@ describe('Interactuando con los elementos', () => {
 		cy.get('.btn.btn-primary').click({ multiple: true })
 	})
 
-	it.only('Click con teclas alternativas', () => {
+	it('Click con teclas alternativas', () => {
 		cy.visit('/buttons')
 		cy.get('button').eq(3).click({
 			shiftKey: true,
@@ -51,5 +51,20 @@ describe('Interactuando con los elementos', () => {
 			// windows o command en mac
 			metaKey: true,
 		})
+	})
+
+	it.only('Inputs type text', () => {
+		cy.visit('/automation-practice-form')
+		cy.get('#firstName').type('Javier')
+		cy.get('#lastName').type('Fuentes')
+		//Observa que pasa si se vuelve a mandar el type , el texto se concatena
+		cy.get('#firstName').type('Javier')
+		//Entonces limpiemos el input
+		cy.get('#firstName').type('{selectAll}{backspace}')
+		cy.get('#firstName').type('Otro nombre')
+		//Otra forma de hacerlo
+		cy.get('#firstName').clear()
+		//Mocernos al otro input
+		cy.get('#firstName').type('Otro nombre{enter}')
 	})
 })
